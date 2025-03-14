84bcd6add9ce575259973f610eee22f3834aebf3
priti-madake87 <pritimadake929@gmail.com>
2025-03-14 16:52:28 -0700

    Undo merge and clean files

--- src/postContextApi/PostContext.jsx
+++ src/postContextApi/PostContext.jsx
@@ -6,7 +6,7 @@ const PostContextProvider = ({ children }) => {
   let Allposts = JSON.parse(JSON.stringify(posts));
   return (
     <PostContext.Provider value={{ Allposts }}>{children}</PostContext.Provider>
-  );
+  );zz
 };
 
 export default PostContextProvider;
